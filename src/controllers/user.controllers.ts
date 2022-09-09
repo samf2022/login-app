eeaf149de5879d0177f6cccc0c57c0b3cbc1e872
Santiago <samf0794@gmail.com>
2022-09-09 09:09:10 -0400

    fix: nodemon base folder changed to src. ref sn

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,7 +1,10 @@
 import { Response, Request } from "express";
 
 const getAllUser = (req: Request, res: Response) => {
-    const data = 'data test'
+    const data = {
+        user: 'Santiago',
+        email: 'santiago@gmail.com'
+    }
     return res.status(200).json({data})
 }
 
