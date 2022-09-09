1e644b238ae73becc60fa7b904fe3a97578c907b
Santiago <samf0794@gmail.com>
2022-09-09 10:29:17 -0400

    feat: adding user schema. ref sn

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,29 @@
+import { Schema, model, Model } from "mongoose";
+import { IUser } from "../interfaces/user.interface";
+
+interface IUserModel extends IUser{}
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const User = model<IUser, IUserModel>('User', userSchema)
+
+export {
+    User
+}
